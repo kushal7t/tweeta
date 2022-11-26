62358294cd8dd6a4bab4872defb0260c974630b8
kushal thakker <kushalthakker7@gmail.com>
2022-11-26 16:50:47 -0500

    beta1

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# Go to https://apps.twitter.com/ and create an app.
+# The consumer key and secret will be generated for you after
+consumer_key="w7Riu6txAY4M3zB67TWq2UBKB"
+consumer_secret="RnkmgEXgyPG2xlXCHLFYCxdwXxiQKsWeeSALVy9aZMBhqmMyxV"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Create New App" section
+access_token="2864499197-eFIyBlD5Tr3egU76v7pN5M7Yr5VPnHS5iYc9sfb"
+access_token_secret="SCqnUIqxjqMsgbGPU2C1RxFwi7DHIbFAsNr4MxcEFtCSI"
